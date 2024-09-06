8f1a6677166bd67614ba90cd545b0f05ebae0927
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-09-06 13:46:58 +0300

    logoutAAsssasc

--- src/features/TodolistsList/Todolist/Task/Task.tsx
+++ src/features/TodolistsList/Todolist/Task/Task.tsx
@@ -14,7 +14,6 @@ type TaskPropsType = {
 }
 export const Task = React.memo((props: TaskPropsType) => {
     const onClickHandler = useCallback(() => props.removeTask(props.task.id, props.todolistId), [props.task.id, props.todolistId]);
-
     const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
         let newIsDoneValue = e.currentTarget.checked
         props.changeTaskStatus(props.task.id, newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New, props.todolistId)
