6b2326ded8c144432901d714d3d83a196a36d659
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-09-17 22:31:20 +0300

    logoutAAssscs!

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,45 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import reportWebVitals from './reportWebVitals';
-// import App from './app/App';
-// import {Provider} from 'react-redux';
-// import {store} from './app/store';
-// import {BrowserRouter, createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
-// import {Login} from "./components/Login/Login";
-// import {TodolistsList} from "./features/TodolistsList/TodolistsList";
-// import {ErrorPage} from "./components/ErrorPage/ErrorPage";
-// const router = createBrowserRouter([
-//     {
-//         path: "/",
-//         element: <App/>,
-//         errorElement: <ErrorPage/>,
-//         children: [
-//             {
-//                 index: true,
-//                 element: <Navigate to="/todolists"/>
-//             },
-//             {
-//                 path: "/login",
-//                 element: <Login/>,
-//             },
-//             {
-//                 path: "/todolists",
-//                 element: <TodolistsList/>,
-//             },
-//         ],
-//     },
-// ]);
-// const root = ReactDOM.createRoot(
-//     document.getElementById('root') as HTMLElement
-// );
-//
-// root.render(
-//     <Provider store={store}>
-//         {/*<App/>*/}
-//         <RouterProvider router={router}/>
-//     </Provider>
-// );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from './app/App';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
+import {BrowserRouter, createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import {Login} from "./components/Login/Login";
+import {TodolistsList} from "./features/TodolistsList/TodolistsList";
+import {ErrorPage} from "./components/ErrorPage/ErrorPage";
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                index: true,
+                element: <Navigate to="/todolists"/>
+            },
+            {
+                path: "/login",
+                element: <Login/>,
+            },
+            {
+                path: "/todolists",
+                element: <TodolistsList/>,
+            },
+        ],
+    },
+]);
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <Provider store={store}>
+        {/*<App/>*/}
+        <RouterProvider router={router}/>
+    </Provider>
+);
 //
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
